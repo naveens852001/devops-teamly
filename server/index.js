a4d152c7245ef60bb53c924fabf77b1c46e632f2
DevopsFarmer <121556299+DevopsFarmer@users.noreply.github.com>
2024-09-19 21:19:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // Serve index.html for all other routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const allowedOrigins = [
